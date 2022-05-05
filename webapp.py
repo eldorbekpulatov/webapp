e70b3d320c05ce5e5c51f4768467181c52d4054f
eldorbekpulatov <eb654@cornell.edu>
2022-05-05 15:42:40 -0400

    Added project gantchart to landing page

--- webapp.py
+++ webapp.py
@@ -1,26 +1,30 @@
 from flask import Flask
 from flask import url_for
-from markupsafe import escape
+from flask import jsonify
+from flask import url_for
 from flask import render_template
+from flask import send_from_directory
+from markupsafe import escape
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static/')
 
+@app.route("/data/<path:filename>")
+def data(filename):
+  return send_from_directory(directory="media", 
+        filename=filename, as_attachment=True)
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-
 @app.route("/tictactoe/", methods=['GET'])
 def tictactoe():
     return render_template('tictactoe.html')
 
-
 @app.route("/triangulation/", methods=['GET'])
 def triangulation():
     return render_template('triangulation.html')
-
 
 @app.route("/sorts/", methods=['GET'])
 def sorts():
@@ -29,8 +33,6 @@ def sorts():
 @app.route("/heatmap/", methods=['GET'])
 def heatmap():
     return render_template('heatmap.html')
-
-
 
 
 if __name__ == '__main__':
